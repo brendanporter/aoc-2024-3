0e02ecf17d10add249195c0be70a96d9781ac6d8
Brendan Porter <bporter@infinitydirect.com>
2025-02-23 13:29:07 -0600

    working on part 2

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ mod tests {
         //     161
         // );
         assert_eq!(
-            multiply("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
+            multiply("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"),
             48
         );
     }
