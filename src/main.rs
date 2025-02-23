b4686adcac9cb4e0cce7b6494d007e1dae07e99c
Brendan Porter <bporter@infinitydirect.com>
2025-02-23 11:13:19 -0600

    first

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use regex::Regex;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file = File::open("input.txt").expect("can't open file");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("cannot read the file");
+
+    let mut product: i32 = 0;
+
+    for line in contents.lines() {
+        product += multiply(line);
+    }
+
+    println!("product is: {}", product)
+}
+
+fn multiply(input: &str) -> i32 {
+    // find all regex matches for 
+    let re = Regex::new("mut\(\d{1,3}+\,\d{1,3}\)").unwrap();
+    
+    let mut results = vec![];
+    for (_, [path, line_no, line]) in re.captures_iter(input).map(|c| c.extract()) {
+        results.push((path, line_no.parse::<u64>()?, line));
+    }
+
+
+    return 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn safe() {
+        assert_eq!(
+            multiply(
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+            ),
+            161
+        );
+    }
+}
