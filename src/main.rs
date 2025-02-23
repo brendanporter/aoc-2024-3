34c3e5e16770bf634d00433e60570489fd48a13f
Brendan Porter <bporter@infinitydirect.com>
2025-02-23 11:49:27 -0600

    stuck

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
 use regex::Regex;
+use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
@@ -12,23 +12,26 @@ fn main() {
     let mut product: i32 = 0;
 
     for line in contents.lines() {
-        product += multiply(line);
+        product += multiply(line)?;
     }
 
     println!("product is: {}", product)
 }
 
-fn multiply(input: &str) -> i32 {
-    // find all regex matches for 
-    let re = Regex::new("mut\(\d{1,3}+\,\d{1,3}\)").unwrap();
-    
+fn multiply(input: &str) -> Option<i32> {
+    // find all regex matches for
+    let re = Regex::new(r"mut\((\d{1,3})\,(\d{1,3})\)").unwrap();
+
     let mut results = vec![];
     for (_, [path, line_no, line]) in re.captures_iter(input).map(|c| c.extract()) {
         results.push((path, line_no.parse::<u64>()?, line));
     }
 
+    for result in results {
+        dbg!(result)
+    }
 
-    return 0
+    return Option<0>;
 }
 
 #[cfg(test)]
@@ -37,9 +40,7 @@ mod tests {
     #[test]
     fn safe() {
         assert_eq!(
-            multiply(
-                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-            ),
+            multiply("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
             161
         );
     }
